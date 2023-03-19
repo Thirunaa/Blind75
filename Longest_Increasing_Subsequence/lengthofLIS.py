0ed4392b0ed69cee8dd3825a2abf55b27aea26aa
Thirunaa <thirunaavukkarasu.thiru@gmail.com>
2023-03-19 13:43:51 -0400

    Updated Longest Increasing Subsequence - Added binary search approach

--- Longest_Increasing_Subsequence/lengthofLIS.py
+++ Longest_Increasing_Subsequence/lengthofLIS.py
@@ -15,3 +15,40 @@ class Solution(object):
                     dp[i] = max(dp[j] + 1, dp[i]) 
     
         return max(dp)
+    
+# Time Complexity : O(nlogn) 
+# Space Complexity : O(n)  
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        dp = [nums[0]]
+
+        for i in range(1, n):
+            if nums[i] > dp[-1]:
+                dp.append(nums[i])
+            else:
+                replace_index = self.binary_search(dp, nums[i])
+                dp[replace_index] = nums[i]
+        
+        return len(dp)
+
+
+    def binary_search(self, nums, target):
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            mid = (l + (r - l) // 2)
+            
+            if nums[mid] == target:
+                return mid
+            if target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+                
+        return l
